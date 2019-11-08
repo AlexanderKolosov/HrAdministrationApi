b17bc6566df460a517624b4990e339a47d8b4adb
AKolosov <alexkp@inbox.ru>
2019-11-08 16:11:20 +0300

    (PRACT - 2) Application start point created

---
+++ SpringBootApi/src/main/java/com/osh/hrApplication.java
@@ -0,0 +1,10 @@
+package com.osh;
+
+import org.springframework.boot.SpringApplication;
+
+public class hrApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(hrApplication.class, args);
+    }
+}
